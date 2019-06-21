8eb5e4ff4f11543bffca3a6463bb828faea29e26
Kevin Guo <Kevin@Kevins-Air.home>
2019-06-20 17:08:01 -1000

    practice more random numbers

--- exercises.js
+++ exercises.js
@@ -15,4 +15,34 @@
 // 9. generates a new score list with a 5% increase for each score 
 // 10.generates a list of scores that are odd numbered
 
+var grades = (function (arry) {
 
+    function findSum() {
+        let sum = 0;
+        for (let i = 0; i < arry.length; i++) {
+            sum = arry[i] + sum;
+        }
+        return sum
+    }
+
+    function findAvg() {
+        let sum = findSum()
+        return sum/arry.length
+    }
+
+    function randomScore() {
+        return arry[Math.floor((Math.random() * arry.length) + 0)]
+        // to return a random number from x to y use Math.floor() to get a non decimal number and ((Math.random() * x) + y)
+    }
+
+    return {
+        findSum: findSum,
+        findAvg: findAvg,
+        randomScore: randomScore,
+        
+    }
+})([65, 76, 100, 88, 17, 55]);
+
+console.log(grades.findSum())
+console.log(grades.findAvg())
+console.log(grades.randomScore())
