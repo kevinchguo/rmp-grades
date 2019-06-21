b3a0f14b34edac6b5bf2ead8edae3004790465cd
Kevin Guo <Kevin@Kevins-Air.home>
2019-06-20 17:59:40 -1000

    do while loop  for random num

--- exercises.js
+++ exercises.js
@@ -35,14 +35,71 @@ var grades = (function (arry) {
         // to return a random number from x to y use Math.floor() to get a non decimal number and ((Math.random() * x) + y)
     }
 
+    function passingScores() {
+        let empArry = []
+        for (let i = 0; i < arry.length; i++) {
+            if (arry[i] >= 70) {
+                empArry.push(arry[i])
+            }
+        }
+        return empArry
+    }
+
+    function firstScore() {
+        return arry[0];
+    }
+
+    function lastScore() {
+        return arry[arry.length - 1]
+    }
+
+    function fivePercentExtraCredit() {
+        let newArry = [];
+        for (let i = 0; i < arry.length; i++) {
+            let newScore = 0;
+            newScore = arry[i] + (Math.round(arry[i] * 0.05));
+            newArry.push(newScore)
+            console.log(newScore)
+        }
+        return newArry;
+    }
+
+    function randomScores() {
+        let newArr = [];
+        // for (let i = 0; i < arry.length; i++) {
+        //     let randomNum = Math.floor((Math.random() * 100) + 0)
+        //     if (randomNum % 2 !== 0 ) {
+        //         newArr.push(randomNum)
+        //     }
+        // }
+        do {
+            let randomNum = Math.floor((Math.random() * 100) + 0)
+            if (randomNum  %2 !== 0) {
+                newArr.push(randomNum)
+            }
+            } while (newArr.length < arry.length);
+        return newArr;
+    }
+        
+
+
     return {
         findSum: findSum,
         findAvg: findAvg,
         randomScore: randomScore,
-        
+        passingScores: passingScores,
+        firstScore: firstScore,
+        lastScore: lastScore,
+        fivePercentExtraCredit: fivePercentExtraCredit,
+        randomScores: randomScores,
     }
 })([65, 76, 100, 88, 17, 55]);
 
 console.log(grades.findSum())
 console.log(grades.findAvg())
 console.log(grades.randomScore())
+console.log(grades.passingScores())
+console.log(grades.firstScore())
+console.log(grades.lastScore())
+console.log(grades.fivePercentExtraCredit())
+console.log(grades.randomScores())
